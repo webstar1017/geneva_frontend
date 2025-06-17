3950345c360df671945033e5378ba2248eb282ab
webstar1017 <webstar1017@hotmail.com>
2025-06-17 03:01:10 -0600

    adjusted

--- src/app/dashboard/index/page.jsx
+++ src/app/dashboard/index/page.jsx
@@ -124,7 +124,7 @@ function Dashboard() {
                     />
                 </div>
             </div>
-            <Item title="Cost to date" value="$375"/>
+            <Item title="Cost to date" value={`$${data.cost_to_date}`}/>
         </div>
     </div>
 }
